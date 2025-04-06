810e60321184a1415a85878c22bf6183d243b31c
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-04-06 20:58:01 +0100

    Added some comments

--- module.py
+++ module.py
@@ -7,6 +7,12 @@ __counter: int = 0
 
 
 def sum_list(the_list: list[int]) -> int:
+    """
+    requires
+        nothing
+    returns
+        the sum of the_list's elements
+    """
     global __counter
     __counter += 1
     sum: int = 0
@@ -17,16 +23,25 @@ def sum_list(the_list: list[int]) -> int:
 
 
 def prod_list(the_list: list[int]) -> int:
+    """
+    requires
+        nothing
+    returns
+        the product of the_list's elements
+    """
     global __counter
     __counter += 1
     prod: int = 1
+
     for element in the_list:
         prod *= element
     return prod
 
 
 if __name__ == "__main__":
+    my_list: list[int]
+
     print("Called directly. Running tests...")
-    my_list: list[int] = [i + 1 for i in range(5)]
+    my_list = [i + 1 for i in range(5)]
     print(sum_list(my_list))
     print(prod_list(my_list))
